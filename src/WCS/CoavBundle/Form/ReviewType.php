4ba8e47ada54fc65fed54d5293b76d69e84a0494
cegaz <cecilegaz@gmail.com>
2017-11-22 16:19:33 +0100

    quete formulaires

--- src/WCS/CoavBundle/Form/ReviewType.php
+++ src/WCS/CoavBundle/Form/ReviewType.php
@@ -4,16 +4,36 @@ namespace WCS\CoavBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
 
 class ReviewType extends AbstractType
 {
     /**
-     * {@inheritdoc}
+     * {@inheritdoc} including all fields from Review Entity
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('text')->add('userRated')->add('reviewAuthor')->add('publicationDate')->add('note');
+        $builder
+            ->add('text', TextareaType::class,
+                ['attr' =>
+                ['maxlength' => 250, 'label' => 'Description']])
+            ->add('publicationDate', DateType::class,
+                ['data' => new \DateTime('now')])
+            ->add('note', IntegerType::class, ['attr' =>
+                ['min' => 0, 'max' => 5, 'label' => 'Note']])
+            ->add('agreeTerms', CheckboxType::class, ['mapped' => false])
+            ->add('userRated', EntityType::class, ['class' => 'WCS\CoavBundle\Entity\User',
+                'query_builder' => function(EntityRepository $er) {
+                return $er->createQueryBuilder('u')->orderBy('u.lastName', 'ASC');
+                },
+                'choice_label' => 'phoneNumber'])
+            ->add('reviewAuthor');
     }
     
     /**
